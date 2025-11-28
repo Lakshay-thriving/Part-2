8fbcbf8fa1f05a50fa4a6adfb2b371c5461187b8
Lakshay Kumar <lakshaykumar.workspace@gmail.com>
2025-11-28 22:25:49 +0530

    Fix: Added actual xv6 source files

---
+++ xv6-labs-2025/kernel/spinlock.h
@@ -0,0 +1,20 @@
+// Mutual exclusion lock.
+struct spinlock {
+  uint locked;       // Is the lock held?
+
+  // For debugging:
+  char *name;        // Name of lock.
+  struct cpu *cpu;   // The cpu holding the lock.
+#ifdef LAB_LOCK
+  int nts;
+  int n;
+#endif
+};
+
+#ifdef LAB_LOCK
+// Reader-writer lock.
+struct rwspinlock {
+  // Replace this with your implementation.
+  struct spinlock l;
+};
+#endif
